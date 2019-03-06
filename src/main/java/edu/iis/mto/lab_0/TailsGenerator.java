f55c21f0f63d2ec30cc87c58de07f6b13a753e6c
Dettlaff.Bartłomiej <209295@edu.p.lodz.pl>
2019-03-06 17:54:10 +0100

    Napisanie metody tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 public class TailsGenerator {
 
+    private List<String> result = new ArrayList<>();
+
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        if(value == null){
+            return result;
+        }
+
+        int sizeOfString = value.length();
+
+        for(int i=0; i<=sizeOfString; i++){
+            result.add(value.substring(i,sizeOfString));
+        }
+
+        return result;
     }
 
 }
